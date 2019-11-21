886ca20ce2b35ee1a1d30886249ce34524f6893c
Ronaldd Pinho <ronaldppinho@gmail.com>
2019-11-21 10:24:38 -0300

    Class SCPFile was added

--- src/scp/SCPFile.h
+++ src/scp/SCPFile.h
@@ -5,12 +5,19 @@
 #ifndef ORLIB_SCPFILE_H
 #define ORLIB_SCPFILE_H
 
+#include "../InstanceFile.h"
 
-
-class SCPFile {
-
+class SCPFile : public InstanceFile {
+public:
+    /*
+     * Default constructor
+     */
+    explicit SCPFile(std::string filename) : InstanceFile(filename) {
+        if (!this->is_open()) {
+            throw std::runtime_error("SCPFile not opened.");
+        }
+    }
 };
-
 
 
 #endif //ORLIB_SCPFILE_H
