1831bb78213a9b5c9af2dae2157b43b6a724d592
Ronaldd Pinho <ronaldppinho@gmail.com>
2019-11-23 10:01:09 -0300

    Add new tests for SCPFile class and compilation script

--- test/test-scpfile.cpp
+++ test/test-scpfile.cpp
@@ -8,13 +8,26 @@
 #include "../src/SCPFile.h"
 
 #include <vector>
+#include <exception>
 
-TEST_SUITE("Files of Set Covering Problem") {
+TEST_SUITE("SCP Files" * doctest::description("Testing files of Set Covering Problem instances")) {
     using std::vector;
 
     TEST_CASE("Instantiation") {
         SCPFile file("files/scp41.txt");
-        CHECK( file.is_open() == true );
+        CHECK_EQ(file.is_open(), true);
+
+        DOCTEST_CHECK_THROWS_WITH(SCPFile("scp41.txt"), "SCPFile not opened.");
+    }
+
+    TEST_CASE("Buffer values") {
+        SCPFile file("files/scp41.txt");
+        file.bufferize();
+
+        vector<int> vec = file.values();
+        CHECK( vec[0] == 200 );
+        CHECK( vec[1] == 1000 );
+        CHECK( vec.size() == 5211 );
     }
 
 }
